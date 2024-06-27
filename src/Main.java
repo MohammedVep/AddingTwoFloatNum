42d22889bd9c338c5cd37bbcd008df43c32019cb
MohammedVep <98326062+MohammedVep@users.noreply.github.com>
2024-06-27 15:08:52 -0400

    Completed Add Two Floating-Point Numbers Exercise

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        float x = 23.4f;
+        float y = 32.6f;
+
+        float sum = x + y;
+        System.out.println(sum);
+    }
+}
